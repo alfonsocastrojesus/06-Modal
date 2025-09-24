f640298f6cf7a4d430b3cbf5b744ed7aaeb87f22
castro_alfonso <alfonso.je.castro@gmail.com>
2025-09-23 22:19:23 -0500

    add initial code

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 'use strict';
+const showModal = document.querySelector('.show-modal');
+const closeModal = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+for (let i = 0; i < showModal.length; i++);
