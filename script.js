ee311cc3a95b9df645ae31b58465af7293e8716e
castro_alfonso <alfonso.je.castro@gmail.com>
2025-09-24 20:28:06 -0500

    add modal functions

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 'use strict';
-const showModal = document.querySelector('.show-modal');
+const showModal = document.querySelectorAll('.show-modal');
 const closeModal = document.querySelector('.close-modal');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 
-for (let i = 0; i < showModal.length; i++);
+for (let i = 0; i < showModal.length; i++)
+  showModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+function closeM() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+modal.addEventListener('click', closeM);
+overlay.addEventListener('click', closeM);
+
+function handleEscapeKey(event) {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) closeM();
+}
+document.addEventListener('keydown', handleEscapeKey);
